140fc8136a2cf21fa49d15086793347e19a86655
ecdsafu <fig@squidrouter.com>
2024-01-03 16:42:54 +1030

    chore: add pretty names to wrappers and dexs

--- src/wrappers/index.ts
+++ src/wrappers/index.ts
@@ -11,11 +11,22 @@ export enum WrapDirection {
   WRAP = "wrap",
   UNWRAP = "unwrap",
 }
+
+export enum WrapperName {
+  WRAPPED_NATIVE = "Native Wrapper",
+  OVERNIGHT = "Overnight Finance",
+  DEUS = "DEUS Finance",
+  WSTETH = "Lido Wrapped stETH",
+  WRAPPED_MAI = "QiDAO Wrapped MAI",
+  WRAPPED_USDY = "Ondo Finance",
+}
+
 export interface Wrapper {
   chainId: string;
   address: string;
   coinAddresses: string[];
   coinNames: string[];
   type: WrapperType;
+  name: WrapperName;
   properties?: Record<string, any>;
 }
