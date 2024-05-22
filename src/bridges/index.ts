0786c6603c63af4fdec3ff4796100ab92cbddcf0
jmd3v <juan.villarraza@gmail.com>
2024-05-22 11:01:29 -0300

    fix: dexes and bridges names

--- src/bridges/index.ts
+++ src/bridges/index.ts
@@ -1,17 +1,17 @@
 export enum BridgeType {
-  AXELAR_GMP = "gmp",
-  AXELAR_ITS = "its",
+  AXELAR_GMP = "axelar-gmp",
+  AXELAR_ITS = "axelar-its",
   CCTP = "cctp",
   NOBLE_CCTP = "noble-cctp",
   IBC = "ibc",
 }
 
 export enum BridgeProvider {
-  AXELAR = "axelar",
-  CCTP = "cctp",
-  NOBLE_CCTP = "noble-cctp",
-  IBC = "ibc",
-  PFM = "pfm",
+  AXELAR = "Axelar",
+  CCTP = "CCTP",
+  NOBLE_CCTP = "Noble CCTP",
+  IBC = "IBC",
+  PFM = "IBC Packet forward middleware",
 }
 
 export type BridgeConfig = Record<string, string>;
