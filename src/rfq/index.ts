28ed7190e68382bd85a2dae7742be1d26e33c015
Cory <146833132+corysquid@users.noreply.github.com>
2025-05-22 14:03:07 -0700

    add `CoralMessageProvider` type (#167)
    * add `CoralMessageProvider` type
    
    * add LayerZeroEndpoint enum

--- src/rfq/index.ts
+++ src/rfq/index.ts
@@ -11,6 +11,33 @@ export enum HubEventType {
   SETTLEMENT_PROCESSED = "SettlementProcessed",
 }
 
+export enum CoralMessageProvider {
+  UNSUPPORTED = "Unsupported",
+  AXELAR = "Axelar",
+  LAYER_ZERO = "LayerZero",
+}
+
+export enum LayerZeroEndpoint {
+  DEFAULT = "0",
+  ETHEREUM = "30101",
+  OPTIMISM = "30111",
+  BINANCE = "30102",
+  GNOSIS = "30145",
+  POLYGON = "30109",
+  SONIC = "30332",
+  FANTOM = "30112",
+  HYPER_EVM = "30367",
+  SONEIUM = "30340",
+  PEAQ = "30302",
+  BASE = "30184",
+  ARBITRUM = "30110",
+  CELO = "30125",
+  AVALANCHE = "30106",
+  LINEA = "30183",
+  BERACHAIN = "30362",
+  BLAST = "30243",
+}
+
 export type Order = {
   fromAddress: string;
   toAddress: string;
