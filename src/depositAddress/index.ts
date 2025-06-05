236941f68316d7e96c2fbe4497726f8a85bb0577
nnoln <48334589+nnoln@users.noreply.github.com>
2025-06-05 10:15:01 -0300

    update deposit address for sol transfer (#169)

--- src/depositAddress/index.ts
+++ src/depositAddress/index.ts
@@ -2,6 +2,7 @@ export interface ChainflipDepositAddressResponse {
   depositAddress: string;
   amount: string;
   chainflipStatusTrackingId: string;
+  data?: string;
 }
 
 export type DepositAddressResponse = ChainflipDepositAddressResponse;
