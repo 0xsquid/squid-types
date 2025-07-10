c75ff4844cf40e3034c1168e4da5aa99469d00c6
Koda <12773720+0x0Koda@users.noreply.github.com>
2025-07-10 17:24:20 +1000

    added kodiak v3 on bera

--- src/dexes/index.ts
+++ src/dexes/index.ts
@@ -31,6 +31,7 @@ export enum DexName {
   KYBERSWAP = "KyberSwap",
   KYBERSWAP_AGGREGATOR = "kyberswap Aggregator",
   KINETIX_V3 = "Kinetix V3",
+  KODIAK_V3 = "Kodiak V3",
   MENTO_V2 = "Mento V2",
   OPENOCEAN = "OpenOcean",
   OSMOSIS = "Osmosis",
